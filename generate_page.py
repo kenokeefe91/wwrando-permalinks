d8b2b9e2b04e76de2f30106353e21bfdf619f764
kenokeefe91 <kenokeefe560@gmail.com>
2023-05-29 18:54:21 +0200

    parse permalink to be url safe and put coop tracker for coop

--- generate_page.py
+++ generate_page.py
@@ -1,14 +1,37 @@
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from urllib import parse
 
 
 def generate_setting_page():
     config = read_json_config("permalinks.json")
     setting_page_template = get_settings_page_template()
 
-    setting_html = setting_page_template.render(config)
+    page_data = generate_page_data_from(config)
+
+    setting_html = setting_page_template.render(page_data)
 
     save_setting_page(setting_html)
+
+
+def generate_page_data_from(config):
+    page_data = {"rando_version": config["rando_version"], "settings": []}
+
+    for setting in config["settings"]:
+        tracker_url = f"https://www.wooferzfg.me/tww-rando-tracker/#/tracker/new/{parse.quote(setting['permalink'], safe='')}"
+
+        if "is_coop" in setting and setting["is_coop"]:
+            tracker_url = "https://jaysc.github.io/tww-rando-tracker-coop"
+
+        page_data["settings"].append(
+            {
+                "name": setting["name"],
+                "permalink": setting["permalink"],
+                "tracker_link": tracker_url,
+            }
+        )
+
+    return page_data
 
 
 def read_json_config(path):
@@ -23,9 +46,11 @@ def get_settings_page_template():
     )
     return template_env.get_template("settings_string.template.html")
 
+
 def save_setting_page(content):
     with open("page/index.html", "w") as setting_page:
         setting_page.write(content)
+
 
 if __name__ == "__main__":
     generate_setting_page()
